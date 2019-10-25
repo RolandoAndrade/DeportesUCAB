96f21bd858be890a0737326274c40aa63daf7707
rolandoandrade <rolandoandradefernandez@gmail.com>
2019-10-25 14:47:32 -0400

    Animaciones en campos de texto

---
+++ public/javascripts/cards.js
@@ -0,0 +1,9 @@
+function splitLabel(container)
+{
+    $(container).find(".text-label").addClass("min");
+}
+function unSplitLabel(container)
+{
+    if($(container).find(".create-input")[0].value.length===0)
+        $(container).find(".text-label").removeClass("min");
+}
