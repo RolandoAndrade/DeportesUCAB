d600975a4e92b40e49e0e6763bd7e2aac847e478
rolandoandrade <rolandoandradefernandez@gmail.com>
2019-10-25 16:52:15 -0400

    Date pickers de materialize

--- public/javascripts/cards.js
+++ public/javascripts/cards.js
@@ -7,3 +7,52 @@ function unSplitLabel(container)
     if($(container).find(".create-input")[0].value.length===0)
         $(container).find(".text-label").removeClass("min");
 }
+
+$(document).ready(function(){
+    $('.fecha-input').datepicker({firstDay:1, format: 'dd-mm-yyyy', i18n:{cancel: "Cancelar", months: [
+            'Enero',
+            'Febrero',
+            'Marzo',
+            'Abril',
+            'Mayo',
+            'Junio',
+            'Julio',
+            'Agosto',
+            'Septiembre',
+            'Octubre',
+            'Noviembre',
+            'Diciembre'
+        ], monthsShort:
+                [
+                    'Ene',
+                    'Feb',
+                    'Mar',
+                    'Abr',
+                    'May',
+                    'Jun',
+                    'Jul',
+                    'Ago',
+                    'Sep',
+                    'Oct',
+                    'Nov',
+                    'Dic'
+                ], weekdays: [
+                'Domingo',
+                'Lunes',
+                'Martes',
+                'Miércoles',
+                'Jueves',
+                'Viernes',
+                'Sábado'
+            ], weekdaysShort: [
+                'Dom',
+                'Lun',
+                'Mar',
+                'Mié',
+                'Jue',
+                'Vie',
+                'Sáb'
+            ], weekdaysAbbrev: ['D','L','M','X','J','V','S'],
+
+    }});
+});
