2c71628524d6041e5e8b19777cc66ba0d6861720
rolandoandrade <rolandoandradefernandez@gmail.com>
2019-10-25 18:08:15 -0400

    Usados los input text de materialize

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -15,7 +15,6 @@ function exitWindow()
 }
 
 
-
 $(document).ready(function(){
 	$('.btn-sideBar-SubMenu').on('click', function(){
 		var SubMenu=$(this).next('ul');
@@ -88,6 +87,55 @@ $(document).ready(function(){
 		$('#map').hide(300);
 		$('.makeACollectionOrder').show(300);
 	});
+
+	$('.fecha-input').datepicker({firstDay:1, format: 'dd-mm-yyyy', i18n:{cancel: "Cancelar", months: [
+				'Enero',
+				'Febrero',
+				'Marzo',
+				'Abril',
+				'Mayo',
+				'Junio',
+				'Julio',
+				'Agosto',
+				'Septiembre',
+				'Octubre',
+				'Noviembre',
+				'Diciembre'
+			], monthsShort:
+				[
+					'Ene',
+					'Feb',
+					'Mar',
+					'Abr',
+					'May',
+					'Jun',
+					'Jul',
+					'Ago',
+					'Sep',
+					'Oct',
+					'Nov',
+					'Dic'
+				], weekdays: [
+				'Domingo',
+				'Lunes',
+				'Martes',
+				'Miércoles',
+				'Jueves',
+				'Viernes',
+				'Sábado'
+			], weekdaysShort: [
+				'Dom',
+				'Lun',
+				'Mar',
+				'Mié',
+				'Jue',
+				'Vie',
+				'Sáb'
+			], weekdaysAbbrev: ['D','L','M','X','J','V','S'],
+
+		}});
+
+	M.updateTextFields();
 
 });
 
