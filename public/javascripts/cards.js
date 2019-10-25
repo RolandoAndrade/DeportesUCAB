86a9921326cabd058da172afa343cb7965764a7c
rolandoandrade <rolandoandradefernandez@gmail.com>
2019-10-25 18:10:31 -0400

    Eliminados métodos osciosos

--- public/javascripts/cards.js
+++
@@ -1,9 +0,0 @@
-function splitLabel(container)
-{
-    //$(container).find(".text-label").addClass("min");
-}
-function unSplitLabel(container)
-{
-    /*if($(container).find(".create-input")[0].value.length===0)
-        $(container).find(".text-label").removeClass("min");*/
-}
