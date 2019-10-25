1b724fbe5b575c98864cf8726b36846d8f585720
rolandoandrade <rolandoandradefernandez@gmail.com>
2019-10-25 19:06:00 -0400

    Añadir cartas de características de manera dinámica

---
+++ public/javascripts/cards.js
@@ -0,0 +1,37 @@
+let create = 0;
+ function addCaracteristica()
+ {
+     $("#caracteristicas-container").append('<div class="create-caracteristica-card">' +
+         '<div class="create-content-container">' +
+         '<div class="create-data-container">' +
+         '<div class="input-container">' +
+         '<div class="input-field">' +
+         '<input id="caracteristica-type'+create+'" type="text" class="create-input validate">' +
+         '<label for="caracteristica-type'+create+'">Título</label>' +
+         '</div>' +
+         '</div>' +
+         '<div class="input-field">' +
+         '<select>' +
+         '<option value="info">Información</option>' +
+         '<option value="fecha">Fecha</option>' +
+         '<option value="tiempo">Tiempo</option>' +
+         '<option value="peligro">Advertencia</option>' +
+         '</select>' +
+         '<label>Descripción</label>' +
+         '</div>' +
+         '</div>' +
+         '</div>' +
+         '<div class="create-content-container">' +
+         '<div class="create-data-container">' +
+         '<div class="input-container">' +
+         '<div class="input-field">' +
+         '<textarea id="textarea1" class="create-input materialize-textarea validate"></textarea>' +
+         '<label for="textarea1">Datos específicos de la competición</label>' +
+         '</div>' +
+         '</div>' +
+         '</div>' +
+         '</div>' +
+         '</div>').hide().fadeIn(300);
+     create++;
+     $('select').formSelect();
+ }
