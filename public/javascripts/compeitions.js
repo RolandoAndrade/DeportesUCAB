1300c6677af97294fba1cab72ba311514bf986cb
rolandoandrade <rolandoandradefernandez@gmail.com>
2019-11-22 22:15:31 -0400

    Generar tabla de posiciones

--- public/javascripts/compeitions.js
+++ public/javascripts/compeitions.js
@@ -18,6 +18,7 @@ function showCompetitionDetail(event)
         $("#event-card-content-detail").find(".card-preview-image").css({"background": "url("+event.imagen+") center"});
         retrieveDetails(event);
         retrieveMatches(event);
+        retrieveClassifications(event);
     }
     else
     {
@@ -50,7 +51,6 @@ function addDetailDescription(data,i)
 }
 function addJornada(jornada)
 {
-    console.log(jornada);
     let aaa = "";
     jornada.forEach((i)=>{
         aaa+=addMatch(i);
@@ -122,10 +122,10 @@ function groupMatches(matches)
     let r = {};
     matches.forEach((i)=>
     {
-        if(r[i.nombre_partido])
-            r[i.nombre_partido].push(i);
+        if(r[i.nombre_partido||i.nombre])
+            r[i.nombre_partido||i.nombre].push(i);
         else
-            r[i.nombre_partido]=[i];
+            r[i.nombre_partido||i.nombre]=[i];
     });
     return r;
 }
@@ -135,10 +135,74 @@ async function retrieveMatches(event)
     $(".partidos-content").empty();
     let partidos = await new GetRequest("/api/v1/events/"+event.id+"/partidos").execute();
     partidos = groupMatches(partidos.data);
-    console.log(partidos);
     for(let i in partidos)
     {
         addJornada(partidos[i]);
+    }
+
+}
+
+function addTeamToClassification(i, pos)
+{
+    let nombre = i.equipo;
+    let escudo = i.escudo;
+    let jugados = i.jugados;
+    let ganados = i.ganados;
+    let perdidos = i.perdidos;
+    let empatados = i.empatados;
+    let gf = i.goles_favor;
+    let gc = i.goles_contra;
+    let s = '<tr>\n' +
+    '         <td>'+pos+'</td>\n' +
+    '         <td class="classification-team-name"><div class="classification-team-shield"><img src="'+escudo+'" alt=""></div>\n' +
+    '             <div>'+nombre+'</div></td>\n' +
+    '         <td>'+jugados+'</td>\n' +
+    '         <td>'+ganados+'</td>\n' +
+    '         <td>'+empatados+'</td>\n' +
+    '         <td>'+perdidos+'</td>\n' +
+    '         <td>'+gf+'</td>\n' +
+    '         <td>'+gc+'</td>\n' +
+        '      <td>'+(ganados*3+empatados)+'</td>\n' +
+    '      </tr>';
+    return s;
+}
+
+function addClasificacion(tablero)
+{
+    let teamsPositions = "";
+    let j = 1;
+    let titulo = tablero[0].nombre;
+    tablero.forEach((i)=>{
+        teamsPositions+=addTeamToClassification(i,j++);
+    })
+    let s = '<div class="post-card results">\n' +
+        '      <div class="result-title">\n' +
+                    titulo+
+        '       </div>'+
+                '<table class="classification-table">\n' +
+                    '                        <tr>\n' +
+                    '                            <th></th>\n' +
+                    '                            <th>EQUIPO</th>\n' +
+                    '                            <th>PJ</th>\n' +
+                    '                            <th>G</th>\n' +
+                    '                            <th>E</th>\n' +
+                    '                            <th>P</th>\n' +
+                    '                            <th>GF</th>\n' +
+                    '                            <th>GC</th>\n' +
+                    '                            <th>PTOS</th>\n' +
+                    '                        </tr>'+
+                    teamsPositions+'</table></div>';
+    $(".clasificacion-content").append(s);
+}
+
+async function retrieveClassifications(event)
+{
+    $(".clasificacion-content").empty();
+    let clasificacion = await new GetRequest("/api/v1/events/"+event.id+"/clasificacion").execute();
+    clasificacion = groupMatches(clasificacion.data);
+    for(let i in clasificacion)
+    {
+        addClasificacion(clasificacion[i]);
     }
 
 }
