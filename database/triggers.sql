dcd72bc839047a2598909ed9cd2fb6e4215a0acc
rolandoandrade <rolandoandradefernandez@gmail.com>
2019-11-23 22:44:47 -0400

    Probando deploy en heroku

--- database/triggers.sql
+++ database/triggers.sql
@@ -1,12 +1,15 @@
 CREATE OR REPLACE FUNCTION cambiaClasificacionTrigger() RETURNS trigger AS
 $$
 BEGIN
-    IF NOT isEnClasificacion(NEW.equipo_local, NEW.clasificacion_id) THEN
-        INSERT INTO TABLA_CLASIFICACION VALUES (DEFAULT, NEW.equipo_local, NEW.clasificacion_id);
-    end if;
-    IF NOT isEnClasificacion(NEW.equipo_visitante, NEW.clasificacion_id) THEN
-        INSERT INTO TABLA_CLASIFICACION VALUES (DEFAULT, NEW.equipo_visitante, NEW.clasificacion_id);
-    end if;
+    IF NEW.clasificacion_id IS NOT NULL THEN
+        IF NOT isEnClasificacion(NEW.equipo_local, NEW.clasificacion_id) THEN
+            INSERT INTO TABLA_CLASIFICACION VALUES (DEFAULT, NEW.equipo_local, NEW.clasificacion_id);
+        end if;
+        IF NOT isEnClasificacion(NEW.equipo_visitante, NEW.clasificacion_id) THEN
+            INSERT INTO TABLA_CLASIFICACION VALUES (DEFAULT, NEW.equipo_visitante, NEW.clasificacion_id);
+        end if;
+    END IF;
+    RETURN NEW;
 END;
 $$ LANGUAGE 'plpgsql';
 
