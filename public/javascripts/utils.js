ec687fa2f1ead8da10c1e745c15ac2919e78dd4e
rolandoandrade <rolandoandradefernandez@gmail.com>
2019-11-26 20:40:21 -0400

    Arreglado bug de fechas

--- public/javascripts/utils.js
+++ public/javascripts/utils.js
@@ -2,10 +2,10 @@ const months = ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto
 
 function dateSimple(date)
 {
-    return (date.getDay()+1)+" de "+months[date.getMonth()]
+    return (date.getDate())+" de "+months[date.getMonth()]
 }
 
 function datePair(date)
 {
-    return (date.getDay()+1)+"/"+(date.getMonth()+1);
+    return (date.getDate())+"/"+(date.getMonth()+1);
 }
