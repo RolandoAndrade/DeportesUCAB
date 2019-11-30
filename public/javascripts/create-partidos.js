1262175bb732ddfa738086ae8206eca513ccb2d4
rolandoandrade <rolandoandradefernandez@gmail.com>
2019-11-30 12:27:17 -0400

    No poder escoger el equipo como local y visitante

--- public/javascripts/create-partidos.js
+++ public/javascripts/create-partidos.js
@@ -27,7 +27,69 @@ async function retrieveTeamsOfCompetition(fase)
 }
 
 
-function addTeams()
+function addTeamsToMatch(caso)
 {
+    let s = "";
+    teamsOfCompetition.forEach((i,k)=>
+    {
+        let escudo = i.escudo;
+        let nombre = i.nombre;
+        let id = i.id;
+        s+='<div class="create-equipo-card">' +
+            '<div class="create-equipo-card-shield">' +
+            '<img src="'+escudo+'" alt="">' +
+            '</div>' +
+            '<div class="create-equipo-card-teamname">' + nombre+
+            '</div>' +
+            '<div class="more-button-green team'+(i.check?" selected":"")+'" onclick="selectTeamToMatch('+k+','+caso+')">' +
+            '<i class="zmdi zmdi-check"></i>' +
+            '</div>' +
+            '</div>';
+    })
+
+    if (teamsOfCompetition.length > 0)
+    {
+        swal({
+            title: 'Selecciona los equipos',
+            confirmButtonText: '<i class="zmdi zmdi-close"></i>  CANCELAR',
+            confirmButtonColor: '#03A9F4',
+            showCancelButton: false,
+            html: s
+        }).then(function () {
+
+        }).catch((e)=>{});
+    }
+}
+
+function selectTeamToMatch(i, caso)
+{
+    swal.closeModal();
+    if(!teamsOfCompetition[i].check)
+    {
+        if(caso===1)
+        {
+            teamsOfCompetition.forEach((i)=>
+            {
+                if(i.local) i.check=false;
+                i.local = false;
+            });
+
+
+        }
+        else
+        {
+            teamsOfCompetition.forEach((i)=>
+            {
+                if(i.visitante) i.check=false;
+                i.visitante = false;
+            });
+        }
+
+        teamsOfCompetition[i].check=true;
+        teamsOfCompetition[i].local=caso===1;
+        teamsOfCompetition[i].visitante=caso===2;
+    }
+
+
 
 }
