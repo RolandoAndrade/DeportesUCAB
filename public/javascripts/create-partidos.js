f949df65fab6869ea2fa2a7373e61867be851c6b
rolandoandrade <rolandoandradefernandez@gmail.com>
2019-11-30 12:00:23 -0400

    Devolver los equipos inscritos

--- public/javascripts/create-partidos.js
+++ public/javascripts/create-partidos.js
@@ -1,3 +1,5 @@
+let teamsOfCompetition = [];
+
 function getFaseFromUrl()
 {
     let url = document.location.href;
@@ -13,5 +15,19 @@ function showCrearPartidos()
     $("#creador-partidos-content").show(300);
     $("#detail-title").html("Crear partidos");
     replaceIcon("back-icon");
-    console.log(getFaseFromUrl())
+    retrieveTeamsOfCompetition(getFaseFromUrl())
+}
+
+async function retrieveTeamsOfCompetition(fase)
+{
+    //let req = await new GetRequest("/api/v1/phases/"+fase.id+"/matches?tipo="+fase.tipo).execute();
+    let req = await new GetRequest("/api/v1/teams/events/"+fase.id).execute();
+    teamsOfCompetition= req.data;
+    console.log(teamsOfCompetition);
+}
+
+
+function addTeams()
+{
+
 }
