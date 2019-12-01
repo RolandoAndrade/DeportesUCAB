cdf2b337e3ecd6efff50a1277354889cccec04de
rolandoandrade <rolandoandradefernandez@gmail.com>
2019-12-01 19:10:09 -0400

    Participación de los jugadores

--- database/triggers.sql
+++ database/triggers.sql
@@ -18,3 +18,23 @@ AFTER INSERT
 ON PARTIDO
 FOR EACH ROW
 EXECUTE PROCEDURE cambiaClasificacionTrigger();
+
+CREATE OR REPLACE FUNCTION agregarjugadoresalpartido() RETURNS trigger AS
+$$
+DECLARE
+    I RECORD;
+BEGIN
+    FOR I IN (SELECT * FROM ATLETA WHERE equipo_id = NEW.equipo_local OR equipo_id = NEW.equipo_visitante)
+    LOOP
+        INSERT INTO JUEGO_FUTBOL VALUES(DEFAULT, 'delantero', 0,0,0,0,0,0,'x',I.id_atleta,NEW.id_partido);
+    END LOOP;
+    RETURN NEW;
+END;
+$$ LANGUAGE 'plpgsql';
+
+CREATE TRIGGER participacionDeEquipos
+    AFTER INSERT
+    ON PARTIDO
+    FOR EACH ROW
+EXECUTE PROCEDURE agregarjugadoresalpartido();
+
