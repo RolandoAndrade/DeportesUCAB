09e96418ff18924f890c98a543fd74078f8c377f
rolandoandrade <rolandoandradefernandez@gmail.com>
2019-12-01 18:22:44 -0400

    Modificar partidos preparado

---
+++ public/javascripts/modificar-partidos.js
@@ -0,0 +1,7 @@
+function showModificarPartidos()
+{
+    hideAll();
+    $("#modificar-partidos-content").show(300);
+    $("#detail-title").html("Modificar partidos");
+    replaceIcon("creador-icon");
+}
