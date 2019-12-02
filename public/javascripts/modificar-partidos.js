af705e75ad030c44cea8930bf2913072b22fa45f
rolandoandrade <rolandoandradefernandez@gmail.com>
2019-12-02 14:50:48 -0400

    Añadidos id al retorno del resultado del partido

--- public/javascripts/modificar-partidos.js
+++ public/javascripts/modificar-partidos.js
@@ -1,7 +1,28 @@
+function getPartidoFromUrl()
+{
+    $("#loader-creador-partidos").hide();
+    let url = document.location.href;
+    let partido = url.substring(url.lastIndexOf("?partido=")+9);
+    return partido;
+}
+
 function showModificarPartidos()
 {
     hideAll();
     $("#modificar-partidos-content").show(300);
     $("#detail-title").html("Modificar partido");
     replaceIcon("back-icon");
+    retrieveMatch();
+}
+
+function showPartidoData(partido)
+{
+    let idlocal = partido.resultado.lo
+}
+
+async function retrieveMatch()
+{
+    let id = getPartidoFromUrl();
+    let partido = (await new GetRequest("/api/v1/matches/"+id).execute()).data;
+    console.log(partido)
 }
