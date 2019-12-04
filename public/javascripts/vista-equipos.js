3b428b525af3788f2051e57ecd28e811c0316c08
rolandoandrade <rolandoandradefernandez@gmail.com>
2019-12-03 21:32:25 -0400

    Ver equipos

--- public/javascripts/vista-equipos.js
+++ public/javascripts/vista-equipos.js
@@ -4,4 +4,39 @@ function showVistaEquipos()
     $("#vista-equipos-content").show(300);
     $("#detail-title").html("Equipos");
     replaceIcon("equipos-icon");
+    retrieveTeamsForView();
+}
+
+function cambiarDetalleEquipo(id)
+{
+
+}
+
+function createViewVistaEquipos(teams)
+{
+    $("#teams-info-container").empty();
+    let s = '';
+    teams.forEach((i)=>
+    {
+        let id = i.id;
+        let nombre = i.nombre;
+        let escudo = i.escudo;
+        let genero = i.genero;
+        s += '              <div class="team-card-info '+genero+'" onclick="cambiarDetalleEquipo('+id+')">\n' +
+            '                    <div class="team-card-info-image">\n' +
+            '                        <img src="'+escudo+'" alt="">\n' +
+            '                    </div>\n' +
+            '                    <div class="player-name">\n' +
+                                    nombre+
+            '                    </div>\n' +
+            '                </div>'
+    });
+    $("#teams-info-container").append(s);
+}
+
+async function retrieveTeamsForView()
+{
+    let teams = (await new GetRequest("/api/v1/teams").execute()).data;
+    console.log(teams)
+    createViewVistaEquipos(teams);
 }
