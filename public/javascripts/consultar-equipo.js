47b901f0f1e119b3a4fa470f9c14278a3b02bfb4
rolandoandrade <rolandoandradefernandez@gmail.com>
2019-12-04 09:54:06 -0400

    Empiezo mostrar información del equipo

--- public/javascripts/consultar-equipo.js
+++ public/javascripts/consultar-equipo.js
@@ -1,8 +1,20 @@
+function getEquipoFromURL()
+{
+    let url = document.location.href;
+    return parseInt(url.substring(url.lastIndexOf("?equipoid=")+10));
+}
+
 function showConsultarEquipos()
 {
     hideAll();
     $("#consultar-equipo-content").show(300);
-    $("#detail-title").html("");
+    $("#detail-title").html("Datos del equipo");
     replaceIcon("back-icon");
-    retrieveTeamsForView();
+    //retrievePlayersOfTeam();
+}
+
+async function retrievePlayersOfTeam()
+{
+    let data = (await new GetRequest("/api/v1/players/teams/"+getEquipoFromURL()).execute()).data;
+
 }
