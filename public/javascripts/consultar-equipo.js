86e06a2704fb7c1739ff95e40f6692cb4ff696b0
rolandoandrade <rolandoandradefernandez@gmail.com>
2019-12-04 10:14:34 -0400

    Mostrar lista de jugadores del equipo

--- public/javascripts/consultar-equipo.js
+++ public/javascripts/consultar-equipo.js
@@ -10,11 +10,40 @@ function showConsultarEquipos()
     $("#consultar-equipo-content").show(300);
     $("#detail-title").html("Datos del equipo");
     replaceIcon("back-icon");
-    //retrievePlayersOfTeam();
+    retrievePlayersOfTeam();
+}
+
+function showPlayersOfTeam(data)
+{
+    let s = '';
+
+
+    $("#tabla-juadores").empty();
+
+    data.forEach((i)=>
+    {
+        let imagen = i.imagen;
+        let nombre = i.nombre + " "+i.apellido;
+        s+='                         <tr>' +
+            '                           <td>\n' +
+            '                                <div class="classification-player-image" style="background-image: url('+imagen+'); width: 80px; height: 80px"></div>\n' +
+            '                            </td>\n' +
+            '                            <td>\n' +
+                                            nombre+
+            '                            </td></tr> '
+    });
+
+    s = '                        <tr>\n' +
+        '                            <th></th>\n' +
+        '                            <th>NOMBRE</th>\n' +
+        '                        </tr>'+s;
+    $("#tabla-juadores").append(s);
 }
 
 async function retrievePlayersOfTeam()
 {
-    let data = (await new GetRequest("/api/v1/players/teams/"+getEquipoFromURL()).execute()).data;
+    let data = (await new GetRequest("/api/v1/players/teams/"+getEquipoFromURL()).execute());
+    console.log(data)
+    showPlayersOfTeam(data.data);
 
 }
