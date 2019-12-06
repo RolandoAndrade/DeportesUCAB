858fcf9194362014990b26814a09ba28ee0d2a40
rolandoandrade <rolandoandradefernandez@gmail.com>
2019-12-05 21:24:08 -0400

    API de agentes

--- routes/api.js
+++ routes/api.js
@@ -696,4 +696,54 @@ router.post('/teams',function (req, res, next)
         })
 });
 
+router.get('/freeagents/:id(\\d+)',function (req, res, next)
+{
+    client.query('SELECT * from obtenerAgentesLibres($1)',
+        [req.params.id],(err, result)=>{
+            if(err)
+            {
+                console.log(err);
+                res.status(500).json(
+                    {
+                        status: 'error',
+                        data: err
+                    });
+            }
+            else
+            {
+
+
+                res.status(200).json({
+                    status: 'success',
+                    data: result.rows
+                });
+            }
+        })
+});
+
+router.get('/privateagents/:id(\\d+)',function (req, res, next)
+{
+    client.query('SELECT * from obtenerAgentesLibres($1)',
+        [req.params.id],(err, result)=>{
+            if(err)
+            {
+                console.log(err);
+                res.status(500).json(
+                    {
+                        status: 'error',
+                        data: err
+                    });
+            }
+            else
+            {
+
+
+                res.status(200).json({
+                    status: 'success',
+                    data: result.rows
+                });
+            }
+        })
+});
+
 module.exports = router;
